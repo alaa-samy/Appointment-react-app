a0133f62f912093d7e3ce82fcf68bf231ef6eee9
Alaa Samy <64163691+alaa-samy@users.noreply.github.com>
2022-05-04 20:45:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useState, useEffect, useCallback } from "react";
+import './styles.css';
 import { BiCalendar } from "react-icons/bi";
 import Search from "./components/Search";
 import AddAppointemnt from "./components/AddAppointment";
 import AppointmentInfo from "./components/AppointmentInfo";
-// import List from './data.json'
 
 function App() {
   let [appointmentList, setAppointmentList] = useState([]);
